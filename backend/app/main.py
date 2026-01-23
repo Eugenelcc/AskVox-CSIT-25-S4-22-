bf3a19044435c917072b8735a9b7f59c5df57b68
Eugene <eugenelcc@hotmail.com>
2026-01-24 06:39:49 +0800

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,7 +30,7 @@ origins = [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:3000",
-    "https://askvox-csit-25-s4-22-1.onrender.com/",
+    "https://askvox-csit-25-s4-22-1.onrender.com",
     # "https://askvox-frontend.vercel.app"  <-- Add your production URL later
 ]
 
